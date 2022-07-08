c362ec29819bb78cdeda55cdedea64c129ffd27f
The-Violinist <violinist.david@yahoo.com>
2022-07-08 09:35:59 -0700

    Pulled rotator from merge_pdfs

--- merge_pdfs/rotator.py
+++ merge_pdfs/rotator.py
@@ -1,16 +1,17 @@
-# from email.policy import strict
 import PyPDF2
 
-pdf_in = open('MAV.pdf', 'rb')
-pdf_reader = PyPDF2.PdfFileReader(pdf_in,strict=False)
-pdf_writer = PyPDF2.PdfFileWriter()
+def rotate():
+    print("Rotating the MAV pdf...")
+    pdf_in = open('MAV.pdf', 'rb')
+    pdf_reader = PyPDF2.PdfFileReader(pdf_in,strict=False)
+    pdf_writer = PyPDF2.PdfFileWriter()
 
-for pagenum in range(pdf_reader.numPages):
-    page = pdf_reader.getPage(pagenum)
-    page.rotateClockwise(90)
-    pdf_writer.addPage(page)
+    for pagenum in range(pdf_reader.numPages):
+        page = pdf_reader.getPage(pagenum)
+        page.rotateClockwise(90)
+        pdf_writer.addPage(page)
 
-pdf_out = open('rotated.pdf', 'wb')
-pdf_writer.write(pdf_out)
-pdf_out.close()
-pdf_in.close()
+    pdf_out = open('mav_rotated.pdf', 'wb')
+    pdf_writer.write(pdf_out)
+    pdf_out.close()
+    pdf_in.close()
